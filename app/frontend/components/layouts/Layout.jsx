92908f9af4ac4f8cff9abe2a62b1dc17ea6f9784
Exequiel Rozas <exequiel98@gmail.com>
2025-07-06 20:44:53 -0400

    Remove flash messages

--- app/frontend/components/layouts/Layout.jsx
+++ app/frontend/components/layouts/Layout.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import Navbar from '../navigation/Navbar'
+import Navbar from '@components/navigation/Navbar'
+import Flash from '@components/Flash'
+import Footer from '@components/navigation/Footer'
 
 export const Layout = ({ children }) => {
-  const flash = useSelector((state) => state.flash)
-
   return (
     <div>
       <Navbar />
-      {flash.success && <p>{flash.success}</p>}
-      {flash.notice && <p>{flash.notice}</p>}
-      {flash.error && <p>{flash.error}</p>}
+      <Flash />
 
       {children}
+      <Footer />
     </div>
   )
 }
