1d530fd0ab88370c46ec2b8573bfc707338662a0
Exequiel Rozas <exequiel98@gmail.com>
2025-07-06 20:45:30 -0400

    Fix import error

--- app/views/pages/home.jsx
+++ app/views/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout } from '@components/Layout'
+import { Layout } from '@components/layouts/Layout'
 import Hero from '@components/pages/home/Hero'
 
 export default function Home() {
