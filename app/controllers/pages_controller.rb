ce3c319db73f691b610b9d08ea8de4937267fce6
Exequiel Rozas <exequiel98@gmail.com>
2025-07-08 18:17:39 -0400

    Resume session for the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  allow_unauthenticated_access only: %i[ home ]
+  before_action :resume_session, only: %i[ home ]
+  
   before_action :use_jsx_rendering_defaults
   superglue_template "application/superglue"
   
