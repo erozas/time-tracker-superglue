59e6bc14842ee1d8e3959bf4f222128972a01643
Exequiel Rozas <exequiel98@gmail.com>
2025-07-08 17:49:51 -0400

    Add presence validation to 'name'

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 end
