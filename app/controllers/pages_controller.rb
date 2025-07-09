88af194d676489a552fcdced06a67bc0491819fd
Exequiel Rozas <exequiel98@gmail.com>
2025-07-08 22:01:10 -0400

    If the user is signed in we perform a redirect to the dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   allow_unauthenticated_access only: %i[ home ]
   before_action :resume_session, only: %i[ home ]
+  before_action :redirect_to_dashboard_if_signed_in
   
   before_action :use_jsx_rendering_defaults
   superglue_template "application/superglue"
   
   def home
+  end
+
+  private
+
+  def redirect_to_dashboard_if_signed_in
+    redirect_to dashboard_index_path if user_signed_in?
   end
 end
