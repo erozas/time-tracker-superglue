ab75a7b0bfecf5fb20cc3a57bceb60a295e7706e
Exequiel Rozas <exequiel98@gmail.com>
2025-07-08 22:00:41 -0400

    Add 'current_user' and 'user_signed_in?' helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  helper_method :current_user, :user_signed_in?
+  
   # Enables Superglue rendering defaults for sensible view directories.
   #
   # without `use_jsx_rendering_defaults`:
@@ -29,4 +31,14 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+  
+  def current_user
+    @current_user ||= Current.user
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
 end
