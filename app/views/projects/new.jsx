c81f7c23916b8b05c48c2dff306756d0c9ae5f72
Exequiel Rozas <exequiel98@gmail.com>
2025-07-19 17:57:04 -0400

    WIP: nested form

--- app/views/projects/new.jsx
+++ app/views/projects/new.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useContent } from '@thoughtbot/superglue'
+import React, { useState, useContext } from 'react'
+import { useContent, NavigationContext } from '@thoughtbot/superglue'
 import { AppLayout } from '../../frontend/components/layouts/AppLayout'
-import { Form, FieldBase, TextArea, SubmitButton } from "@components/Inputs"
-import { Dialog } from '@components/Dialog'
+import { Form, FieldBase, TextArea, SubmitButton } from "../../frontend/components/Inputs"
+import { Plus, Trash2 } from 'lucide-react'
 
 export default function ProjectsNew() {
   const { form, extras, inputs, errors } = useContent().new_project_form
-  const { createProjectModal } = useContent()
+  const { remote } = useContext(NavigationContext)
 
   console.log(inputs)
   return (
@@ -18,18 +18,27 @@ export default function ProjectsNew() {
             <FieldBase {...inputs.name} errorKey="name" label="Name" className="px-3 py-2 border border-gray-300 rounded-lg" />
             <TextArea {...inputs.description} placeholder="Describe the project in a few words" label="Description" className="px-3 py-2 border border-gray-300 rounded-lg placeholder:text-sm" />
             <div className="flex flex-col space-y-2">
-              <h2 className="text-lg font-semibold">Tasks</h2>
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Tasks</h2>
+                <a data-sg-remote="true" href="/projects/add_task" className="text-indigo-800 font-semibold px-2 flex items-center space-x-1">
+                  <Plus className="w-4 h-4" />
+                  <span className="text-sm">Add task</span>
+                </a>
+              </div>
               <div className="flex flex-col space-y-2">
                 {inputs.tasksAttributes.map((task, index) => (
-                  <div className="flex flex-col space-y-2" key={index}>
+                  <div className="flex flex-row space-x-1 items-center" key={index}>
                     <FieldBase {...task.title} errorKey="title" placeholder="Task Title" className="px-3 py-2 border border-gray-300 rounded-lg" />
                     <FieldBase {...task.allotedTime} type="number" errorKey="allotedTime" placeholder="Alloted Time" className="px-3 py-2 border border-gray-300 rounded-lg" />
+                    <button className="text-gray-500 cursor-pointer">
+                      <Trash2 className="w-5 h-5" />
+                    </button>
                   </div>
                 ))}
               </div>
             </div>
           </div>
-          <SubmitButton text="Create Project" className="bg-indigo-700 text-white font-semibold mt-6 py-2 px-6 rounded-full" />
+          <SubmitButton text="Create Project" className="bg-indigo-700 text-white font-semibold mt-8 py-2 px-6 rounded-full" />
         </Form>
       </div>
     </AppLayout>
