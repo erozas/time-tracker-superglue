4e0d254ea8df124acbb717378dc400e68cf4f252
Exequiel Rozas <exequiel98@gmail.com>
2025-07-19 18:51:57 -0400

    Wrap renderToString with the 'setHumidRenderer' function

--- app/frontend/server_rendering.js
+++ app/frontend/server_rendering.js
@@ -6,21 +6,37 @@ import { buildVisitAndRemote } from './application_visit';
 import { pageIdentifierToPageComponent } from './page_to_page_mapping';
 import { store } from './store';
 
-// Export a function Workers can call:
-export async function renderPage(pathname, baseUrl) {
-  // 1. Fetch the JSON props from your Rails app:
-  const resp = await fetch(`${baseUrl}${pathname}.json`);
-  const initialPage = await resp.json();
+require("source-map-support").install({
+  retrieveSourceMap: filename => {
+    return {
+      url: filename,
+      map: readSourceMap(filename)
+    };
+  }
+});
 
-  // 2. Render to an HTML string:
+setHumidRenderer((json, baseUrl, path) => {
+  const initialState = JSON.parse(json)
   return renderToString(
     <Application
       className="full-height"
+      // The base url prefixed to all calls made by the `visit`
+      // and `remote` thunks.
       baseUrl={baseUrl}
-      initialPage={initialPage}
-      visit={buildVisitAndRemote({ baseUrl, store })}
-      pageIdentifierToPageComponent={pageIdentifierToPageComponent}
+      // The global var SUPERGLUE_INITIAL_PAGE_STATE is set by your erb
+      // template, e.g., application/superglue.html.erb
+      initialPage={initialState}
+      // The initial path of the page, e.g., /foobar
+      path={path}
+      // Callback used to setup visit and remote
+      buildVisitAndRemote={buildVisitAndRemote}
+      // Callback used to setup the store
       store={store}
-    />
-  );
-}
+      // Mapping between the page identifier to page component
+      mapping={pageIdentifierToPageComponent}
+    />,
+    {
+      concurrentFeatures: false,
+    }
+  )
+})
