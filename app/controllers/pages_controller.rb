92b7cf9dcc6001922825201bd0e485a3f08a08e6
Exequiel Rozas <exequiel98@gmail.com>
2025-07-19 17:54:43 -0400

    Remove redirect to dashboard when signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   allow_unauthenticated_access only: %i[ home ]
   before_action :resume_session, only: %i[ home ]
-  before_action :redirect_to_dashboard_if_signed_in
   
   before_action :use_jsx_rendering_defaults
   superglue_template "application/superglue"
