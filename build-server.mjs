2661445eebf4311dad742a17aff7711c1eb21ee5
Exequiel Rozas <exequiel98@gmail.com>
2025-07-19 17:57:32 -0400

    Modify build SSR script

--- build-server.mjs
+++ build-server.mjs
@@ -1,24 +1,28 @@
-import * as esbuild from 'esbuild'
-import { polyfillNode } from "esbuild-plugin-polyfill-node";
+import * as esbuild from 'esbuild';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
 
 await esbuild.build({
   entryPoints: ['app/frontend/server_rendering.js'],
   bundle: true,
-  platform: "browser",
+  platform: 'browser',
   define: {
-    "process.env.NODE_ENV": '"production"'
+    'process.env.NODE_ENV': '"production"',
   },
   sourcemap: true,
   outfile: 'app/assets/builds/server_rendering.js',
-  logLevel: "info",
+  logLevel: 'info',
   loader: {
     ".js": "jsx",
-    ".svg": "dataurl"
+    '.png': 'dataurl',
+    '.jpg': 'dataurl',
+    '.gif': 'dataurl',
+    '.svg': 'dataurl',
+    '.svg': 'dataurl',
   },
-  inject: ["./shim.js"],
+  inject: ['./shim.js'],
   plugins: [
     polyfillNode({
-      globals: false
+      globals: false,
     }),
-  ]
-})
+  ],
+});
