2c3ce3804cbdbff06fc10b5799cf86a647ee5fd7
Exequiel Rozas <exequiel98@gmail.com>
2025-07-20 22:03:31 -0400

    Change background to zinc-50

--- app/frontend/components/layouts/AppLayout.jsx
+++ app/frontend/components/layouts/AppLayout.jsx
@@ -5,7 +5,7 @@ import Flash from "../Flash"
 
 export const AppLayout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-zinc-100">
+    <div className="min-h-screen bg-zinc-50">
       <div className="flex">
         <AppSidebar />
         <main className="w-full">
